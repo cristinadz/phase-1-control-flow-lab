261838f3d83278e7170f6771cde6dcdf70a83624
+ <diazcristina.la@gmail.com>
2022-04-25 16:23:00 -0700

    commit tree 5cfdc78c446c510a296a12d8e4a47e9ddff81edb

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet >= 400 && feet <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (feet >= 2000 && feet <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let results = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return results;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let tipCharm;
+  switch (tip) {
+    case "generous":
+      tipCharm = "Thank you so much.";
+      break;
+    case "not as generous":
+      tipCharm = "Thank you.";
+      break;
+    default:
+      tipCharm = "Bye.";
+      break;
+  }
+  return tipCharm;
 }
